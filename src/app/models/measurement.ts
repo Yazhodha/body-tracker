d8fd469c2f633ce31e8db2638a2098dff3abb712
yazhodha <yazhodha@gmail.com>
2024-12-22 20:59:44 +0530

    Update measurement interface

--- src/app/models/measurement.ts
+++ src/app/models/measurement.ts
@@ -1,2 +1,13 @@
 export interface Measurement {
-}
+    id?: number;
+    date: Date;
+    neck: number;
+    upperArm: number;
+    chest: number;
+    waist: number;
+    hips: number;
+    wrist: number;
+    thighs: number;
+    calves: number;
+    ankles: number;
+  }
