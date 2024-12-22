dbae16c5a68b05b5baf1967df4898de0681f4e25
yazhodha <yazhodha@gmail.com>
2024-12-22 21:02:15 +0530

    Implement measurement-form

--- src/app/components/measurement-form/measurement-form.component.ts
+++ src/app/components/measurement-form/measurement-form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MeasurementService } from '../../services/measurement.service';
 
 @Component({
   selector: 'app-measurement-form',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./measurement-form.component.scss']
 })
 export class MeasurementFormComponent {
+  measurementForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private measurementService: MeasurementService
+  ) {
+    this.measurementForm = this.fb.group({
+      date: [new Date(), Validators.required],
+      neck: ['', [Validators.required, Validators.min(0)]],
+      upperArm: ['', [Validators.required, Validators.min(0)]],
+      chest: ['', [Validators.required, Validators.min(0)]],
+      waist: ['', [Validators.required, Validators.min(0)]],
+      hips: ['', [Validators.required, Validators.min(0)]],
+      wrist: ['', [Validators.required, Validators.min(0)]],
+      thighs: ['', [Validators.required, Validators.min(0)]],
+      calves: ['', [Validators.required, Validators.min(0)]],
+      ankles: ['', [Validators.required, Validators.min(0)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.measurementForm.valid) {
+      this.measurementService.addMeasurement(this.measurementForm.value);
+      this.measurementForm.reset({ date: new Date() });
+    }
+  }
 }
